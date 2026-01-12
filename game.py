50175b131f1ea678f6b96bd31ceffd9f5f0db06e
ozgurmola <ozgurmola10@gmail.com>
2026-01-12 20:18:22 +0300

    Update game.py

--- game.py
+++ game.py
@@ -6,35 +6,71 @@ import random
 #screen.draw.text(str(number))
 
 TITLE="SELAM"
-WIDTH = 500
-HEIGHT= 500
+WIDTH = 1000
+HEIGHT= 1050
 FPS=30
+
+
+
 
 hero = Actor ("hero") 
 final_boss = Actor ("final_boss")
 bg =Actor ("lanetli_orman") 
- 
+
+hero.health =100
+hero.attack = 5
+
+wolfs= []
+count = random.randint(2, 7)
+for i in range(count):
+    x = random.randint(200, 800)
+    y = random.randint(200, 800)
+    enemy = Actor("kurt", (x, y))
+    wolfs.append(enemy)
 
 
 def draw():
+    global wolfs
     bg.draw()
     hero.draw()
+    for i in range(len(wolfs)):
+        wolfs[i].draw()
+    screen.draw.text("HP:", center=(25, 1000), color = 'white', fontsize = 40)
+    screen.draw.text(str(hero.health), center=(75, 1000), color = 'white', fontsize = 40)
+    screen.draw.text("AP:", center=(375, 1000), color = 'white', fontsize = 40)
+    screen.draw.text(str(hero.attack), center=(425, 1000), color = 'white', fontsize = 40)
+ 
 
 
 
 
+def update(dt):
+
+    old_x = hero.x
+    old_y = hero.y
+    if keyboard.left and hero.x > 50:
+        hero.x -= 5
+    elif keyboard.right and hero.x < 950:
+        hero.x += 5
+    elif keyboard.up and hero.y > 50:
+        hero.y -= 5
+    elif keyboard.down and hero.y < 950:
+        hero.y += 5
 
 
 
 
+enemy_index = hero.collidelist(wolfs)
+if enemy_index != -1:
+    hero.x = old_x
+    hero.y = old_y
+    enemy = wolfs[enemy_index]
+    enemy.health -= hero.attack
+    hero.health -= enemy.attack
+    if enemy.health <0:
+        wolfs.pop(enemy_index)
 
-#düşman oluşturma
-#enemies = []
-#for i in range(5):
-    
-    #enemy = Actor("kurt", topleft = (x, y))
-    #enemy.health = random.randint(10, 20)
-    #enemy.attack = random.randint(5, 10)
-    #enemies.append(enemy)
+
 
 pgzrun.go()
+
