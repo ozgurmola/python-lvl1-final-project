ae2a42e5b8d125f9b5a128e0c53c0d8c8b26e93d
ozgurmola <ozgurmola10@gmail.com>
2026-01-12 19:27:34 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,40 @@
+import pgzrun
+import random
+
+#Karakterler
+#number = 4
+#screen.draw.text(str(number))
+
+TITLE="SELAM"
+WIDTH = 1000
+HEIGHT= 1000
+FPS=30
+
+hero = Actor ("hero") 
+final_boss = Actor ("final_boss")
+bg =Actor ("lanetli_orman") 
+ 
+
+
+def draw():
+    bg.draw()
+    hero.draw()
+
+
+
+
+
+
+
+
+
+#düşman oluşturma
+#enemies = []
+#for i in range(5):
+    
+    #enemy = Actor("kurt", topleft = (x, y))
+    #enemy.health = random.randint(10, 20)
+    #enemy.attack = random.randint(5, 10)
+    #enemies.append(enemy)
+
+pgzrun.go()
